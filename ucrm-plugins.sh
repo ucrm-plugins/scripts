354d9554b4dcadceff6043eecb38847bf7698056
Ryan Spaeth <106448843+spaethtech@users.noreply.github.com>
2024-01-04 13:35:23 -0600

    Update ucrm-plugins.sh

--- ucrm-plugins.sh
+++ ucrm-plugins.sh
@@ -3,6 +3,10 @@
 # Make sure GH_TOKEN is set before execution
 
 cd ~
-mkdir -p ucrm-plugins
+mkdir -p spaethtech
+gh repo clone spaethtech/php-monorepo -- --recurse-submodules
 
-gh repo clone ucrm-plugins/monorepo.git -- --recurse-submodules
+
+cd ~
+mkdir -p ucrm-plugins
+gh repo clone ucrm-plugins/monorepo -- --recurse-submodules
