b90a715daa5b7b31c6f4c943da7187b1fb3ba8d8
Ryan Spaeth <106448843+spaethtech@users.noreply.github.com>
2024-01-04 19:21:43 -0600

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # PHP 8.1
-apt-get update
-apt-get install -y ca-certificates apt-transport-https software-properties-common
-add-apt-repository -y ppa:ondrej/php
-apt-get update
-apt install -y php8.1-cli php8.1-curl php8.1-xml php8.1-zip
+sudo apt-get update
+sudo apt-get install -y ca-certificates apt-transport-https software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt install -y php8.1-cli php8.1-curl php8.1-xml php8.1-zip
 
 # Composer (Latest)
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # GitHub CLI
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
@@ -18,3 +18,5 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+echo "export GH_TOKEN=<GitHub Token>" >> ~/.profile
